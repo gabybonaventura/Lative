ec009e0ffc20cf3775c153d4ec34b2e8c5479b97
gabybonaventura <gabybonaventura@gmail.com>
2023-06-03 16:01:40 +0200

    separate the logic and add more test

--- Lative.Application.Tests/CsvProcessorTests.cs
+++ Lative.Application.Tests/CsvProcessorTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 namespace Lative.Domain.Tests;
 
+[TestFixture]
 public class CsvProcessorTests
 {
     private CsvProcessor csvProcessor;
@@ -13,6 +14,101 @@ public class CsvProcessorTests
     {
         csvProcessor = new CsvProcessor(new IndexMap());
         csvFilePath = Path.Combine(TestContext.CurrentContext.TestDirectory, "sample_data_customer_1.csv");
+    }
+
+    [Test]
+    public void ParseSaleOperationModel_ValidFields_ReturnsSaleOperationModel()
+    {
+        // Arrange
+        var fields = new[]
+        {
+            "3707384-15976-1",
+            "667332",
+            "USD",
+            "15,020.00",
+            "31/10/2023",
+            "United States",
+            "FS AM Enterprise",
+            "Recurring Adjustment",
+            "Recurring Adjustment",
+            "PCM",
+            "DealCloud",
+            "DealCloud",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023"
+        };
+
+        // Act
+        var saleOperationModel = csvProcessor.ParseSaleOperationModel(fields);
+
+        // Assert
+        Assert.That(saleOperationModel.OpportunityID, Is.EqualTo("3707384-15976-1"));
+        Assert.That(saleOperationModel.OpportunityOwner, Is.EqualTo("667332"));
+        Assert.That(saleOperationModel.Currency, Is.EqualTo("USD"));
+        Assert.That(saleOperationModel.Amount, Is.EqualTo(15020.00f));
+        Assert.That(saleOperationModel.CloseDate, Is.EqualTo(new DateOnly(2023, 10, 31)));
+    }
+
+    [Test]
+    public void ProcessDimensions_ValidFields_AddsDimensionsToSaleOperationModel()
+    {
+        // Arrange
+        var saleOperationModel = new SaleOperationModel();
+        var fields = new[]
+        {
+            "3707384-15976-1",
+            "667332",
+            "USD",
+            "15,020.00",
+            "31/10/2023",
+            "United States",
+            "FS AM Enterprise",
+            "Recurring Adjustment",
+            "Recurring Adjustment",
+            "PCM",
+            "DealCloud",
+            "DealCloud",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023",
+            "",
+            "31/10/2023"
+        };
+
+        // Act
+        csvProcessor.ProcessDimensions(saleOperationModel, fields);
+
+        // Assert
+        Assert.That(saleOperationModel.Dimensions.Count, Is.EqualTo(6));
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("Location"), Is.True);
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("Segment"), Is.True);
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("Category"), Is.True);
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("ACV Type"), Is.True);
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("Subvertical"), Is.True);
+        Assert.That(saleOperationModel.Dimensions.ContainsKey("Product Family"), Is.True);
     }
 
     [Test]
@@ -35,19 +131,5 @@ public class CsvProcessorTests
         Assert.That(saleOperationModels[1].Currency, Is.EqualTo("USD"));
         Assert.That(saleOperationModels[1].Amount, Is.EqualTo(1890.00f));
         Assert.That(saleOperationModels[1].CloseDate, Is.EqualTo(new DateOnly(2023, 10, 1)));
-    }
-
-    [Test]
-    public void ReadCsv_WithValidCsvFile_ParsesDimensionsCorrectly()
-    {
-        // Act
-        var saleOperationModels = csvProcessor.ReadCsv(csvFilePath).ToList();
-
-        // Assert
-        Assert.That(saleOperationModels.Count, Is.EqualTo(2));
-        
-        Assert.That(saleOperationModels[0].Dimensions.Count, Is.EqualTo(7)); // Expect 7 dimensions
-
-        Assert.That(saleOperationModels[1].Dimensions.Count, Is.EqualTo(7)); // Expect 7 dimensions
     }
 }
--- Lative.Application/CsvProcessor.cs
+++ Lative.Application/CsvProcessor.cs
@@ -12,72 +12,80 @@ public class CsvProcessor : ICsvProcessor
     }
     public IEnumerable<SaleOperationModel> ReadCsv(string filePath)
     {
-        using (var parser = new TextFieldParser(filePath) {TextFieldType = FieldType.Delimited, Delimiters = new[] {","}})
+        using (var parser = new TextFieldParser(filePath) { TextFieldType = FieldType.Delimited, Delimiters = new[] { "," } })
         {
-            //header
             var line = parser.ReadLine();
             _indexMap.SetIndexMap(line);
-            
+
             while (!parser.EndOfData)
             {
                 var fields = parser.ReadFields();
-
-                var saleOperationModel = new SaleOperationModel
-                {
-                    OpportunityID = fields[_indexMap.GetIndex("Opportunity ID")],
-                    OpportunityOwner = fields[_indexMap.GetIndex("Opportunity Owner")],
-                    Currency = fields[_indexMap.GetIndex("Currency")]
-                };
-                if (DateOnly.TryParse(fields[_indexMap.GetIndex("Close Date")], out var closeDate))
-                {
-                    saleOperationModel.CloseDate = closeDate;
-                }
-                else
-                {
-                    // TODO: Log the error and move the line to an error file
-                    continue;
-                }
-                
-                if (float.TryParse(fields[_indexMap.GetIndex("Amount")], out var amount))
-                {
-                    saleOperationModel.Amount = amount;
-                }
-                else
-                {
-                    // TODO: Log the error and move the line to an error file
-                    continue;
-                }
-                
-                
-                // Process dimensions
-                foreach (var dimensionHeader in _indexMap.GetDimensionHeaders())
-                {
-                    var dimensionValue = fields[dimensionHeader.NameIndex];
-                    var startDate = fields[dimensionHeader.StartDateIndex];
-                    var endDate = fields[dimensionHeader.EndDateIndex];
-
-                    if (!string.IsNullOrEmpty(dimensionValue))
-                    {
-                        var dimensionModel = new SaleDimensionModel
-                        {
-                            Name = dimensionHeader.Name,
-                            Value = dimensionValue
-                        };
-
-                        if (DateOnly.TryParse(startDate, out var start))
-                        {
-                            dimensionModel.StartDate = start;
-                        }
-
-                        if (DateOnly.TryParse(endDate, out var end))
-                        {
-                            dimensionModel.EndDate = end;
-                        }
-
-                        saleOperationModel.Dimensions.Add(dimensionHeader.Name, dimensionModel);
-                    }
-                }
+                var saleOperationModel = ParseSaleOperationModel(fields);
+                ProcessDimensions(saleOperationModel, fields);
                 yield return saleOperationModel;
+            }
+        }
+    }
+
+    public SaleOperationModel ParseSaleOperationModel(string[] fields)
+    {
+        var saleOperationModel = new SaleOperationModel
+        {
+            OpportunityID = fields[_indexMap.GetIndex("Opportunity ID")],
+            OpportunityOwner = fields[_indexMap.GetIndex("Opportunity Owner")],
+            Currency = fields[_indexMap.GetIndex("Currency")]
+        };
+
+        if (DateOnly.TryParse(fields[_indexMap.GetIndex("Close Date")], out var closeDate))
+        {
+            saleOperationModel.CloseDate = closeDate;
+        }
+        else
+        {
+            // TODO: Log the error and move the line to an error file
+            return null;
+        }
+
+        if (float.TryParse(fields[_indexMap.GetIndex("Amount")], out var amount))
+        {
+            saleOperationModel.Amount = amount;
+        }
+        else
+        {
+            // TODO: Log the error and move the line to an error file
+            return null; 
+        }
+
+        return saleOperationModel;
+    }
+
+    public void ProcessDimensions(SaleOperationModel saleOperationModel, string[] fields)
+    {
+        foreach (var dimensionHeader in _indexMap.GetDimensionHeaders())
+        {
+            var dimensionValue = fields[dimensionHeader.NameIndex];
+            var startDate = fields[dimensionHeader.StartDateIndex];
+            var endDate = fields[dimensionHeader.EndDateIndex];
+
+            if (!string.IsNullOrEmpty(dimensionValue))
+            {
+                var dimensionModel = new SaleDimensionModel
+                {
+                    Name = dimensionHeader.Name,
+                    Value = dimensionValue
+                };
+
+                if (DateOnly.TryParse(startDate, out var start))
+                {
+                    dimensionModel.StartDate = start;
+                }
+
+                if (DateOnly.TryParse(endDate, out var end))
+                {
+                    dimensionModel.EndDate = end;
+                }
+
+                saleOperationModel.Dimensions.Add(dimensionHeader.Name, dimensionModel);
             }
         }
     }
